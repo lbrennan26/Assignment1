ce9fab7c3a20679d66293dd4139f6d8a2a534589
Lucas Brennan <lub21@pitt.edu>
2016-01-31 20:06:40 -0500

    Added comments

--- BookStore.java
+++ BookStore.java
@@ -14,7 +14,7 @@ public class BookStore
       double subtotal;
       double total;
       int choice;
-      double bookPrice;
+      double bookPrice;             // delcares each variable
       double paintingPrice;
       double bookmarkPrice;
       double bookmarkPackPrice;
@@ -27,24 +27,24 @@ public class BookStore
       boolean again;
       double tax;
 
-        do{
-          do{
+        do{    // do while loop for the whole program
+          do{ // same loop for just the initial if else statement
 
-            System.out.println("More customers in line? (1=yes and 2=no)!");
+            System.out.println("More customers in line? (1=yes and 2=no)!"); // finds out if there are any customers on line
             number = sc.nextInt();
 
-         } while( number != 1 && number != 2 );
+         } while( number != 1 && number != 2 ); // input validation
 
-        if(number == 2)
+        if(number == 2) // if there are no more customers in line it closes
         {
           System.out.println("No more customers we are closing down!");
         }
         else
         {
 
-          do{
+          do{ // heart of te program makes sure the switch will keep going until they checkout
             System.out.println("1 - Buy Books - $5.00 each");
-            System.out.println("2 - Buy Bookmarks - $1.00 each, $5.00 for a six-pack");
+            System.out.println("2 - Buy Bookmarks - $1.00 each, $5.00 for a six-pack");   //UI the customer will see each time
             System.out.println("3 - Buy Paintings of Books - $100.00 each");
             System.out.println("4 - See current order");
             System.out.println("5 - Checkout");
@@ -53,28 +53,28 @@ public class BookStore
 
               switch(choice)
               {
-                case 1: System.out.println("Currently in cart: " + numBooks + " books");
+                case 1: System.out.println("Currently in cart: " + numBooks + " books"); // if they want to purchase books
                         System.out.println("How many books would you like to buy?");
                         numBooks = sc.nextInt();
                         break;
-                case 2: System.out.println("Currently in cart: " + numBookmarks + " bookmarks");
+                case 2: System.out.println("Currently in cart: " + numBookmarks + " bookmarks"); //bookmarks
                         System.out.println("How many bookmarks would you like to buy?");
                         numBookmarks = sc.nextInt();
                         break;
-                case 3: System.out.println("Currently in cart: " + numPaintings + " paintings");
+                case 3: System.out.println("Currently in cart: " + numPaintings + " paintings"); //paintings
                         System.out.println("How many paintings of books would you like to buy?");
                         numPaintings = sc.nextInt();
                         break;
-                case 4: System.out.println("You currently have " + numBooks + " books");
+                case 4: System.out.println("You currently have " + numBooks + " books"); //shows what the current order is
                         System.out.println("You currently have " + numBookmarks + " bookmarks");
                         System.out.println("You currently have " + numPaintings + " paintings of books");
                         break;
-                case 5: discount++;
+                case 5: discount++; // keeps track of the amount of customers
 
-                  if (numBookmarks >= 6)
+                  if (numBookmarks >= 6) // to determine the amount of packs vs single bookmarks
                   {
                    bookmarkPacks = (numBookmarks / 6);
-                   singleBookmarks = (numBookmarks % 6);
+                   singleBookmarks = (numBookmarks % 6); // gives us the remainder
                   }
                 else
                   {
@@ -82,7 +82,7 @@ public class BookStore
                   }
 
 
-                bookPrice = numBooks * 5;
+                bookPrice = numBooks * 5; // gets the price of each variable
                 paintingPrice = numPaintings * 100;
                 bookmarkPrice = singleBookmarks * 1;
                 bookmarkPackPrice = bookmarkPacks * 5;
@@ -91,6 +91,7 @@ public class BookStore
                 {
                   System.out.println("You got a discount!");
                   subtotal = (bookPrice + paintingPrice + bookmarkPrice + bookmarkPackPrice) * .80;
+                  // calucualtes discount
                   tax = subtotal * .07;
                   total = subtotal +tax;
 
@@ -99,7 +100,7 @@ public class BookStore
                 {
 
                   System.out.println("You did not get a discount, maybe next time!");
-                  subtotal = (bookPrice + paintingPrice + bookmarkPrice + bookmarkPackPrice);
+                  subtotal = (bookPrice + paintingPrice + bookmarkPrice + bookmarkPackPrice); //calcutates total price
                   tax = .07 * subtotal;
                   total = subtotal + tax;
 
@@ -109,13 +110,13 @@ public class BookStore
 
 
                 System.out.println("----------------------------------------------");
-                System.out.printf(bookmarkPacks + " Bookmark Pack(s): " + "$%,.2f%n", bookmarkPackPrice); //printf here
-                System.out.printf(numBooks + " Book(s): " + "$%,.2f%n", bookPrice);//printf here
-                System.out.printf(numPaintings + " Painting(s): " + "$%,.2f%n", paintingPrice); //printf here
-                System.out.printf(singleBookmarks + " Single Bookmark(s): " + "$%,.2f%n", bookmarkPrice); //printf here
-                System.out.printf("Your subtotal before tax is: $%,.2f%n", subtotal); //printf here
-                System.out.printf("Your tax is: $%,.2f%n", tax); //printf here
-                System.out.printf("Total is: $%,.2f%n", total);  //printf here
+                System.out.printf(bookmarkPacks + " Bookmark Pack(s): " + "$%,.2f%n", bookmarkPackPrice); //printf showing the price of each
+                System.out.printf(numBooks + " Book(s): " + "$%,.2f%n", bookPrice);
+                System.out.printf(numPaintings + " Painting(s): " + "$%,.2f%n", paintingPrice);
+                System.out.printf(singleBookmarks + " Single Bookmark(s): " + "$%,.2f%n", bookmarkPrice);
+                System.out.printf("Your subtotal before tax is: $%,.2f%n", subtotal);
+                System.out.printf("Your tax is: $%,.2f%n", tax);
+                System.out.printf("Total is: $%,.2f%n", total);
                 System.out.println("----------------------------------------------");
 
                 do{
@@ -126,7 +127,7 @@ public class BookStore
                     if (money > total)
                     {
                       change = money - total;
-                      System.out.printf("Thank you your change is: $%,.2f%n", change); //printf here
+                      System.out.printf("Thank you your change is: $%,.2f%n", change); //makes sure they enter ebough money and gives change
                       System.out.println("Thanks for shopping at this fine location!");
 
                     }
